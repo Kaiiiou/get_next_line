a1082d1dfa9d82e0e179f305b2bbfae54380ce18
Alois <amarti@student.42lehavre.fr>
2025-02-05 06:03:03 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,57 @@
 /*   By: amarti <amarti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 00:41:04 by amarti            #+#    #+#             */
-/*   Updated: 2025/02/02 10:49:14 by amarti           ###   ########.fr       */
+/*   Updated: 2025/02/04 12:00:21 by amarti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *remplissage_svgrd(int fd, char *svgrd, char *buffer)
-{
+// char *fill_line_buffer(int fd, char *svgrd_c)
+// {
+// 	ssize_t read;
+// 	char *temp;
+// 	char *buf;
+
+// 	read = 1;
+// 	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+// 	if(!buf)
+// 		return(NULL);
+// }
+
+// char *get_next_line(int fd)
+// {
+// 	static int *left_c;
+// 	char *temp;
+// 	char *line;
+
+// 	if(fd < 0 || BUFFER_SIZE <= 0)
+// 		return(NULL);
 	
-}
+// 	left_c = fill_line_buffer(fd, left_c);
+// 	if(!left_c)
+// 		return(NULL);
+
+// }
 
 char *get_next_line(int fd)
 {
-	static int svgrd;
-	int temp;
+	CHECK MEM + FD
 
-	if(fd <= 0 || BUFFER_SIZE <= 0)
-		return(NULL);
-	
-	svgrd = ;
+	CALL *fill_line_buffer TO READ --> '\0' || '\n'
+
+	FILL BUFFER_SIZE PER BUFFER_SIZE set_line 
+
+	WHEN set_line IS FULL, line PICK STR WITH THE \n AND LET "surplus" IN set_line COZ static fonction
+
+
+	LINE PUT \0 AFTER STR AND RETURN IT
 }
+
+char *_fill_line_buffer(int fd, char *left_c, char *buffer)
+{
+	
+}
+
+
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: amarti <amarti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 00:41:01 by amarti            #+#    #+#             */
-/*   Updated: 2025/02/02 08:52:57 by amarti           ###   ########.fr       */
+/*   Updated: 2025/02/03 04:32:36 by amarti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
 #ifndef	BUFFER_SIZE 42
 #define BUFFER_SIZE 42
